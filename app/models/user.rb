f353da10e812cf5d2e33c82f11204881ee8dce7d
Jonathan Rose <jonr87ee@gmail.com>
2015-07-17 10:10:59 -0400

    Updated styling for edits and shows

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,4 @@ class User < ActiveRecord::Base
 
   has_many :startups
 
-  # def role
-  # 	if 
-  # 	self.role = "editor"
-  # end
-
-  # def editor? 
-  #   self.role == "editor" 
-  # end
-
 end
